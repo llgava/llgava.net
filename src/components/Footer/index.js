cfd1f59f35c12fe7c73ebf58fb3f97635f43c8f2
Leonardo Luiz Gava <hi@llgava.net>
2020-11-18 18:21:09 -0400

    Add images to manifest

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
-import { FaTwitter, FaFacebookF, FaGithub } from 'react-icons/fa'
+import { FaTwitter, FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa'
 
 import { Container, ContactContainer } from './styles';
 
@@ -11,6 +11,7 @@ function Footer() {
     <Container>
       <a href='https://twitter.com/llgava' target='_blank'><FaTwitter size={iSize} /></a>
       <a href='https://www.facebook.com/gava00' target='_blank'><FaFacebookF size={iSize} /></a>
+      <a href='/#' target='_blank'><FaLinkedinIn size={iSize} /></a>
       <a href='https://github.com/llgava' target='_blank'><FaGithub size={iSize} /></a>
 
       <ContactContainer>
